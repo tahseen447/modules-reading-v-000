beb0c760f6b37883dd7e146d2d6163c4930178e5
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-12 23:23:52 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,4 +4,8 @@ require_relative './class_methods_module.rb'
 class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+
+  def initialize(name)
+    @name = name
+  end
 end
